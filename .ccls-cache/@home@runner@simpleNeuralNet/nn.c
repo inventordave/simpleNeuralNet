95c81733f0cfb0a6360abc5cdb5717129237c81d
Dave <digital.artist@hotmail.co.uk>
2024-12-12 13:34:46 +0000

    snapshot (unfinished)

--- .ccls-cache/@home@runner@simpleNeuralNet/nn.c
+++ .ccls-cache/@home@runner@simpleNeuralNet/nn.c
@@ -1,15 +1,35 @@
 // NEURAL_NET_C
 
 #include <stdlib.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <math.h>
 #include <assert.h>
+#include <string.h>
 
 #define DEVMODE
 
-static uint32_t *** NN;
+#define NN_Ref *(NN+(x*y))
+
+static int * NN;
+
 signed init( unsigned base, unsigned depth	);
+
+char* getstring( char* _ )	{
+
+	char* s = (char*)malloc( (sizeof(char)* strlen(_)) + 1 );
+
+	int i = 0;
+	while( *_ != '\0' )	{
+		
+		s[i++] = *_;
+		_++;
+	}
+
+	s[i] = '\0';
+
+	return s;
+}
+
 
 int main( int argc, char** argv )	{
 
@@ -24,52 +44,93 @@ signed init( unsigned base, unsigned depth	)	{
 
 	int numNodes = pow(base,depth);
 	
-	NN = (uint32_t***) malloc( sizeof(uint32_t) * numNodes  );
+	NN = (int*) malloc( sizeof(int) * numNodes + 1  );
 
-	uint32_t x = base;
-	uint32_t y = depth;
+	int x = 1;
+	int y = 1;
+	int z;
+	char* _1 = (char*) calloc( 1, sizeof(char) * depth + 1 );
+	char* _2 = (char*) calloc( 1, sizeof(char) * depth + 1 );
 
-	for( y; y>0; y-- )	{
-
-		for( x; x>0; x-- )	{
-
-			uint32_t* val = (uint32_t*)malloc( sizeof( uint32_t) );
-			*val = (uint32_t)y * (uint32_t)x;
-			
-			//*(NN + (y*x) ) = val;
-			*NN[y][x] = *val;
-		}
-	}
-
-	assert( x==0 );
-	assert( y==0 );
+	char* strArray[ numNodes ];
 	
-	int z=0;
-	while( 1 )	{
+	strArray[ 0 ] = "0";
 
-		while( y<depth )	{
-			
-			printf( "[%d]:", (int)y );
-			
-			while( x<base )	{
+/** 
+	// 1. if increment [offset] is n % 2 == 1, last digit is 1
+
+	// 2. if ancestor highest-order digit is 1, then current
+	// highest-order digit is 1.
+
+	// 3. if ancestor highest-order digit is 2, then current
+	// highest-order digit is 2.
+
+	// 4. if current node is lhs, it's lowest-order digit is 1.
+	// 5. if current node is rhs, it's lowest-order digit is 2.
+*/
+	
+	int flag = 0;
+	int i = 1;
+	while( y <= depth )	{
+
+		// numSibs := 2^y
+		// 1/2 start with 1, 1/2 start with 2
+		// 2 are 1-based, then 2 2-based, then ...
+
+		char* t1;
+		char* t2;
+		char* lhs_1 = (char*) calloc( 1,2 );
+		lhs_1[0] = '1';
+		char* rhs_1 = (char*) calloc( 1,2 );
+		rhs_1[0] = '1';
+		char* lhs_2 = (char*) calloc( 1,2 );
+		lhs_2[0] = '2';
+		char* rhs_2 = (char*) calloc( 1,2 );
+		rhs_2[0] = '2';
+		
+		for( int j=0; j< pow(2, y ); j++ )	{
+
+			if( !flag )	{
+
+				flag = 1;
+
+				strcpy( t1, lhs_1 ); free( lhs_1 );
+				strcat( t1, "1" );
+				strArray[ i++ ] = getstring( t1 );
+				lhs_1 = getstring( t1 );
+
+				strcpy( t1, rhs_1 ); free( rhs_1 );
+				strcat( t1, "2" );
+				strArray[ i++ ] = getstring( t1 );
+				rhs_1 = getstring( t1 );
 				
-				printf( " %d: %d,", base-x, (int)*(NN[x][y]) );
-				x++;
-				z++;
 			}
-			x=0;
-			y++;
-			printf( "\n\n" );
-		}
+			else	{
+				
+				flag = 0;
 
-		printf( "Should have printed %d entries. Printed %d nodes.\n", numNodes, z );
+				strcpy( t2, lhs_2 ); free( lhs_2 );
+				strcat( t2, "1" );
+				strArray[ i++ ] = getstring( t2 );
+				lhs_2 = getstring( t2 );
 
-		break;
+				strcpy( t2, rhs_2 ); free( rhs_2 );
+				strcat( t2, "2" );
+				strArray[ i++ ] = getstring( t2 );
+				rhs_2 = getstring( t1 );
+
+			}
+			//loop( base(x), exp(y)	) // until exp(y) > depth
 	}
 
-	return z - numNodes;
+	for( i=0; i<numNodes; i++ )	{
+		
+		printf( "%d:\t%s\n", i, strArray[i] );
+		fflush( stdout );
+	}
+	
+	
+	return numNodes - z;
 }
-
-
 
 // end_of_file
--- nn.c
+++ nn.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <assert.h>
+#include <string.h>
 
 #define DEVMODE
 
@@ -12,6 +13,23 @@
 static int * NN;
 
 signed init( unsigned base, unsigned depth	);
+
+char* getstring( char* _ )	{
+
+	char* s = (char*)malloc( (sizeof(char)* strlen(_)) + 1 );
+
+	int i = 0;
+	while( *_ != '\0' )	{
+		
+		s[i++] = *_;
+		_++;
+	}
+
+	s[i] = '\0';
+
+	return s;
+}
+
 
 int main( int argc, char** argv )	{
 
@@ -26,43 +44,92 @@ signed init( unsigned base, unsigned depth	)	{
 
 	int numNodes = pow(base,depth);
 	
-	NN = (int*) malloc( sizeof(int) * numNodes  );
+	NN = (int*) malloc( sizeof(int) * numNodes + 1  );
 
-	int x = base;
-	int y = depth;
+	int x = 1;
+	int y = 1;
+	int z;
+	char* _1 = (char*) calloc( 1, sizeof(char) * depth + 1 );
+	char* _2 = (char*) calloc( 1, sizeof(char) * depth + 1 );
 
-	for( y; y>0; y-- )	{
+	char* strArray[ numNodes ];
+	
+	strArray[ 0 ] = "0";
 
-		for( x; x>0; x-- )	{
+/** 
+	// 1. if increment [offset] is n % 2 == 1, last digit is 1
 
-			int val = y * x;
-			NN_Ref = val;
-		}
-	}
+	// 2. if ancestor highest-order digit is 1, then current
+	// highest-order digit is 1.
 
-	int z=0;
-	while( 1 )	{
+	// 3. if ancestor highest-order digit is 2, then current
+	// highest-order digit is 2.
 
-		while( y<depth )	{
-			
-			printf( "[%d]\n\n", y );
-			
-			while( x<base )	{
+	// 4. if current node is lhs, it's lowest-order digit is 1.
+	// 5. if current node is rhs, it's lowest-order digit is 2.
+*/
+	
+	int flag = 0;
+	int i = 1;
+	while( y <= depth )	{
+
+		// numSibs := 2^y
+		// 1/2 start with 1, 1/2 start with 2
+		// 2 are 1-based, then 2 2-based, then ...
+
+		char* t1;
+		char* t2;
+		char* lhs_1 = (char*) calloc( 1,2 );
+		lhs_1[0] = '1';
+		char* rhs_1 = (char*) calloc( 1,2 );
+		rhs_1[0] = '1';
+		char* lhs_2 = (char*) calloc( 1,2 );
+		lhs_2[0] = '2';
+		char* rhs_2 = (char*) calloc( 1,2 );
+		rhs_2[0] = '2';
+		
+		for( int j=0; j< pow(2, y ); j++ )	{
+
+			if( !flag )	{
+
+				flag = 1;
+
+				strcpy( t1, lhs_1 ); free( lhs_1 );
+				strcat( t1, "1" );
+				strArray[ i++ ] = getstring( t1 );
+				lhs_1 = getstring( t1 );
+
+				strcpy( t1, rhs_1 ); free( rhs_1 );
+				strcat( t1, "2" );
+				strArray[ i++ ] = getstring( t1 );
+				rhs_1 = getstring( t1 );
 				
-				printf( "%d: %d,", base-x, NN_Ref );
-				x++;
-				z++;
 			}
-			x=0;
-			y++;
-			printf( "\n\n" );
-		}
+			else	{
+				
+				flag = 0;
 
-		printf( "Should have printed %d entries. Printed %d nodes.\n", numNodes, z );
+				strcpy( t2, lhs_2 ); free( lhs_2 );
+				strcat( t2, "1" );
+				strArray[ i++ ] = getstring( t2 );
+				lhs_2 = getstring( t2 );
 
-		break;
+				strcpy( t2, rhs_2 ); free( rhs_2 );
+				strcat( t2, "2" );
+				strArray[ i++ ] = getstring( t2 );
+				rhs_2 = getstring( t1 );
+
+			}
+			//loop( base(x), exp(y)	) // until exp(y) > depth
 	}
 
+	for( i=0; i<numNodes; i++ )	{
+		
+		printf( "%d:\t%s\n", i, strArray[i] );
+		fflush( stdout );
+	}
+	
+	
 	return numNodes - z;
 }
 
